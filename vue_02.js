70bacbcce4b58855e28d4f1920350067e17a6382
saviotp <106559606+saviotp@users.noreply.github.com>
2025-04-08 15:11:53 -0300

    Update vue_02.js

--- vue_02.js
+++ vue_02.js
@@ -2,7 +2,9 @@ const { createApp, ref } = Vue
 
 const app = createApp({
     setup() {
+      const marcado = ref(false)
       const count = ref(0)
+      const novaNota = ref(0)
       const alunos = ref([
       {
         nome: 'Bruno', nota: 10
@@ -10,10 +12,21 @@ const app = createApp({
       {
         nome: 'Mateus', nota: 8
       }])
+
+      function lancarNota() {
+        alunos.value.push({
+          nome: 'Fulano',
+          nota: novaNota.value
+        })
+        novaNota.value = null
+      }
       
       return {
         count,
-        alunos
+        alunos,
+        novaNota,
+        lancarNota,
+        marcado
       }
     }
   }).mount('#app')
